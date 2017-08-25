95704429c1ddff98683a75cd5f7810c5801c4d63
Pedro Benevides <pedrobenevidesb@gmail.com>
2017-08-25 13:52:40 -0300

    First project commit

---
+++ Automata01/Automata01.Core/Entidades/Automata.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Automata01.Core.Entidades
+{
+    public sealed class Automata
+    {
+        public Automata(IList<char> alphabet, IList<string> grammar, IDictionary<int, Node> nodes)
+        {
+            Alphabet = alphabet;
+            Grammar = grammar;
+            Nodes = nodes;
+        }
+
+        public IList<char> Alphabet { get; }
+        public IList<string> Grammar { get; }
+        public IDictionary<int, Node> Nodes { get; }
+
+        public bool IsValidSequence(IList<string> input)
+        {
+            for (var i = 0; i < input.Count; i++)
+            {
+                //TODO
+            }
+
+            return true;
+        }
+    }
+
+    public class Node
+    {
+        private readonly Func<string, Direction> _action;
+
+        public Node(string stateValue, Func<string, Direction> action)
+        {
+            _action = action;
+            StateValue = stateValue;
+        }
+
+        public Direction Direction { get; set; }
+        public string StateValue { get; private set; }
+
+        public Direction Transition(string value) => _action(value);
+    }
+
+    public enum Direction
+    {
+        Left,
+        Right,
+        None
+    }
+
+    public class Main
+    {
+        public void Teste()
+        {
+            var q0 = new Node("q0", s =>
+            {
+                if (s == "a")
+                    return Direction.None;
+
+                if (s == "b")
+                    return Direction.Right;
+
+                throw new Exception("Letra não reconhecida pelo alfabeto ou por esse estado.");
+            });
+
+            var q1 = new Node("q1", s =>
+            {
+                if (s == "a")
+                    return Direction.Left;
+
+                if (s == "b")
+                    return Direction.None;
+
+                if (s == "c")
+                    return Direction.Right;
+
+                throw new Exception("Letra não reconhecida pelo alfabeto.");
+            });
+
+            var automata = new Automata(new List<char> { 'a', 'b', 'c' }, new List<string>(), new Dictionary<int, Node> { { 0, q0 }, { 1, q1 } });
+
+        }
+    }
+}
---
+++ Automata01/Automata01.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Automata01.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Automata01.Core")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6f47392a-d392-474c-9397-f9efc9829eec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
